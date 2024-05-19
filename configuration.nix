a0d4a40ae7f6f1d20b4def51404458b0779cc21a
Jason Mishike <jasonmishi@gmail.com>
2024-05-19 16:28:50 +0530

    add pipewire and pavucontrol

--- configuration.nix
+++ configuration.nix
@@ -52,6 +52,17 @@ in
     displayManager.defaultSession = "none+qtile";
   };
 
+  # for pipewire
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
+
   programs = {
     git.enable = true;
   };
@@ -86,7 +97,8 @@ in
   # $ nix search wget
   environment.systemPackages = with pkgs; [
      neofetch
-     
+     pavucontrol
+
      kitty
      floorp
      google-chrome
