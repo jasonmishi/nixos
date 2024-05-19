0bc3e6f2499fd8b5419b3c39e4393c10dae1bca3
Jason Mishike <jasonmishi@gmail.com>
2024-05-19 16:38:35 +0530

    add zotero, obsidian, calibre

--- configuration.nix
+++ configuration.nix
@@ -100,6 +100,7 @@ in
 
      pavucontrol
      cmus
+     nicotine-plus
 
      kitty
      floorp
@@ -119,6 +120,11 @@ in
 	  }
         ];
      })
+
+     zotero
+     obsidian
+     calibre
+
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
