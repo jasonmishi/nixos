8897ba020a2dcc8a6f6e8345a9ebf87ce587ac4c
jasonmishi <jasonmishi@gmail.com>
2024-09-21 11:59:26 +0530

    add neovim to system packages

--- configuration.nix
+++ configuration.nix
@@ -222,6 +222,7 @@ in {
     google-chrome
 
     vscode-fhs # vscode with fhs for extensions1
+    neovim
 
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
